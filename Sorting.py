3894e4e8eba150ccc93422f7e62feba5beaef144
micaris <mkosinowo1@sheffield.ac.uk>
2019-07-16 05:22:44 +0100

    merge sort and quick sort added

---
+++ Sorting.py
@@ -0,0 +1,54 @@
+#Bubble sort
+def bubble_sort(a_list):
+    for pass_num in range(len(a_list) - 1, 0, -1):
+        for i in range(pass_num):
+            if a_list[i] > a_list[i + 1]:
+                temp = a_list[i]
+                a_list[i] = a_list[i + 1]
+                a_list[i + 1] = temp
+
+
+def selection_sort(a_list):
+    for fill_slot in range(len(a_list) -1 , 0, 1):
+        pos_of_max = 0
+        for location in range(1, fill_slot + 1):
+            if a_list[location] > a_list[pos_of_max]:
+                pos_of_max = location
+        
+        temp = a_list[fill_slot]
+        a_list[fill_slot] = a_list[pos_of_max]
+        a_list[pos_of_max] = temp
+
+def insertion_sort(a_list):
+    for index in range(1, len(a_list)):
+        current_value = a_list[index]
+        position = index
+
+        while position > 0 and a_list[position - 1] > current_value:
+            a_list[position] = a_list[position - 1]
+            position = position - 1
+
+        a_list[position] = current_value
+
+
+def shell_sort(a_list):
+    sublist_count = len(a_list) // 2
+    while sublist_count > 0:
+        for start_position in range(sublist_count):
+            gap_insertion_sort(a_list, start_position, sublist_count)
+
+        print("After increments of size", sublist_count, "The list is", a_list)
+        sublist_count = sublist_count // 2
+
+def gap_insertion_sort(a_list, start, gap) :
+
+    for i in range(start + gap, len(a_list), gap):
+        current_value = a_list[i]
+        position = i
+
+        while position >= gap and a_list[position - gap] >  current_value:
+            a_list[position] = a_list[position - gap]
+            position = position - gap
+            a_list[position ] = current_value
+
+def merge_sort(a_list):
